87ce2138b09a2beef1cb7e45c34acb3f3bac5ad1
Matias Hernandez <matiasfh@gmail.com>
2017-11-30 17:56:22 -0300

    fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,52 @@ import babel from 'rollup-plugin-babel'
 import flow from 'rollup-plugin-flow'
 import closure from 'rollup-plugin-closure-compiler-js'
 import uglify from 'rollup-plugin-uglify'
+import cjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
+import path from 'path'
 
-export default {
+const config = {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs',
-  },
+  external: ['react', 'emotion', 'normalize.css'],
+  exports: 'named',
+  sourcemap: true,
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.js',
+      format: 'es',
+    },
+  ],
   plugins: [
     flow(),
     babel({
       exclude: 'node_modules/**',
     }),
     'external-helpers',
-    closure(),
-    uglify(),
+    cjs({ exclude: [path.join(__dirname, '*/src/**/*')] }),
   ],
 }
+
+if (process.env.UMD) {
+  config.globals = {
+    react: 'React',
+  }
+
+  config.plugins.push(closure(), uglify())
+  /*replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),*/
+
+  config.output = [
+    {
+      file: './dist/index.umd.min.js',
+      format: 'umd',
+      name: 'emotion-grid',
+    },
+  ]
+}
+
+export default config
